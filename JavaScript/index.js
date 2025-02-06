d6a63e8ea879ed2e659a53d31d30e0abe55d1cc9
Vicky Sani <vickysani@Vickys-MacBook-Air.fritz.box>
2025-02-06 16:17:01 +0100

    1st version

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,6 @@
 "use strict";
+
+//Hearts
 document.addEventListener("DOMContentLoaded", () => {
   const hearts = document.querySelectorAll(".heart");
   hearts.forEach((heart, index) => {
@@ -6,4 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
       heart.classList.add("visible");
     }, index * 500);
   });
+});
+
+//Modal
+document.getElementById("openModal").addEventListener("click", function () {
+  document.getElementById("modal").classList.remove("hidden");
+});
+
+document.getElementById("closeModal").addEventListener("click", function () {
+  document.getElementById("modal").classList.add("hidden");
 });
